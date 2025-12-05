09255891468ec7204f9b9343479300d24d23617f
leila237 <mbiydzenyuyeileen@gmail.com>
2025-12-05 09:51:31 +0100

    added day 3 to live array

--- constant/challengeDays.ts
+++ constant/challengeDays.ts
@@ -1,5 +1,8 @@
 export const days = [
   { day: 1, name: 'Snowfall Landing', live: true },
   { day: 2, name: 'Santa Chat', live: true },
-  { day: 3, name: 'Elf Name Generator', live: false }
+  { day: 3, name: 'Count Down', live: true },
+  { day: 4, name: 'Elf Name Generator', live: false },
+  { day: 5, name: 'Greeting Card Maker', live: false },
+  { day: 6, name: 'Tree Decorator', live: false }
 ]
