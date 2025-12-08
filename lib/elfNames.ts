263aeab6fb75a295c8d32289c448bb5cac7b8907
leila237 <mbiydzenyuyeileen@gmail.com>
2025-12-08 10:33:57 +0100

    created 3 different arrays for the elf names for the random generation

---
+++ lib/elfNames.ts
@@ -0,0 +1,45 @@
+export const elfFirstNames = [
+  'Jingle',
+  'Pepper',
+  'Twinkle',
+  'Snowy',
+  'Tinsel',
+  'Cookie',
+  'Frosty',
+  'Sparkle',
+  'Holly',
+  'Cocoa',
+  'Mistle',
+  'Blizzard',
+  'Berry',
+  'Nippy'
+]
+
+export const elfMiddleNames = [
+  'Sugar',
+  'Candy',
+  'Ginger',
+  'Pudding',
+  'Snowflake',
+  'Star',
+  'Icicle',
+  'Cinnamon',
+  'Cocoa',
+  'Snowdrop',
+  'Mint',
+  'Plum'
+]
+
+export const elfLastNames = [
+  'McSprinkle',
+  'Winterwhirl',
+  'Peppermint',
+  'Frostwhisper',
+  'Tinselshine',
+  'Snowpockets',
+  'Hollybuttons',
+  'Jollytwist',
+  'Merryboots',
+  'Sparklenose',
+  'Icicletwirl'
+]
